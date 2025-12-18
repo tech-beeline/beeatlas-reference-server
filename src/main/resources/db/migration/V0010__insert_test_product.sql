feb8b185d436ee7bd82127926f2e1ae02d9d1ba7
salikhanov <SAlikhanov@oms.beeline.ru>
2025-12-18 19:10:08 +0600

    insert_test_product

---
+++ src/main/resources/db/migration/V0010__insert_test_product.sql
@@ -0,0 +1,12 @@
+INSERT INTO products.product (name,
+                             alias,
+                             description,
+                             structurizr_api_key,
+                             structurizr_api_secret,
+                             created_date)
+VALUES ('Тестовое приложение',
+        'test',
+        'Тестовое приложение компании',
+        '10476ad7-2675-49e1-861d-6bf2d7964164',
+        '8a02614c-17db-4f7f-bfac-c24ad069ca2q',
+        NOW());
